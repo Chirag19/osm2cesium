46d8c76f02bad8f7371599886526d7a18bd10af8
stirringhalo <stirringhalo@users.noreply.github.com>
2016-07-03 09:34:12 -0400

    Update get_dependencies.sh

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -1,6 +1,6 @@
 # Works on Ubuntu 16.04 at least
 
-sudo apt-get install osmosis nodejs python-virtualenv python-pip # You may need a later version of nodejs
+sudo apt-get install osmosis nodejs python-virtualenv python-pip # You may need a later version of nodejs if you run into a "Method not found error"
 git clone https://github.com/tordanik/OSM2World.git
 git clone https://github.com/AnalyticalGraphicsInc/OBJ2GLTF.git
 virtualenv ~/venv
