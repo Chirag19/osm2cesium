9dd5748d3e338b60d0e1fee0429a71f12ce1dacb
stirringhalo <stirringhalo@users.noreply.github.com>
2016-07-03 10:19:23 -0400

    Update osm2cesium.sh

--- osm2cesium.sh
+++ osm2cesium.sh
@@ -1,8 +1,18 @@
-wget http://download.bbbike.org/osm/bbbike/Zuerich/Zuerich.osm.pbf
-osmosis --read-pbf file=Zuerich.osm.pbf --tf accept-ways "building=*" --write-pbf file=buildings.osm.pbf
+wget http://download.geofabrik.de/europe/azores-latest.osm.pbf
+osmosis --read-pbf file=azores-latest.osm.pbf --tf accept-ways "building=*" --write-pbf file=buildings.osm.pbf
 OSM2World/./osm2world.sh -i buildings.osm.pbf -o test.obj --config prop.properties
 mkdir output
 node OBJ2GLTF/bin/obj2gltf.js test.obj output/model.gltf
 
-echo "Make sure the resulting model is less than 5 MB or so. Not sure what the .gltf limit is in CesiumJS"
-echo "Manually open the resulting fragmentShader0.glsl and modify the diffuseLight and ambientLight vectors to both be (0.9,0.9,0.9)
+LATITUDE=$(grep "Coordinate" test.obj | cut -d " " -f 6 | cut -d "," -f 1)
+LONGITUDE=$(grep "Coordinate" test.obj | cut -d " " -f 8 | cut -d "," -f 1)
+
+sed -i '/fromDegrees/c\Cesium.Cartesian3.fromDegrees($LONGITUDE,$LATITUDE,0));' /var/www/html/cesium/Apps/demo.html
+
+sudo cp -R output /var/www/html/cesium/Apps/SampleData/
+sudo chown -R www-data:www-data  /var/www/html/cesium/Apps/SampleData/output
+
+firefox localhost/cesium/Apps/demo.html
+
+
+
