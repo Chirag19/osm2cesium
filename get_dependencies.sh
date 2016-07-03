ba2673203b80fc40b131b5982a0860fd1067a104
stirringhalo <stirringhalo@users.noreply.github.com>
2016-07-03 09:33:25 -0400

    Update get_dependencies.sh

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -1,7 +1,7 @@
 # Works on Ubuntu 16.04 at least
 
 sudo apt-get install osmosis nodejs python-virtualenv python-pip # You may need a later version of nodejs
-git clone https://github.com/stirringhalo/OSM2World.git
+git clone https://github.com/tordanik/OSM2World.git
 virtualenv ~/venv
 source ~/venv/bin/activate
 pip install mercantile
