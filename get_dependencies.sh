f39f6dcc6a1947bef685d25ebd615bb2c34d8d6a
stirringhalo <stirringhalo@users.noreply.github.com>
2016-07-03 09:53:49 -0400

    Install Cesium

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -1,6 +1,10 @@
 # Works on Ubuntu 16.04 at least
 
-sudo apt-get install osmosis nodejs python-virtualenv python-pip # You may need a later version of nodejs if you run into a "Method not found error"
+sudo apt-get install osmosis nodejs python-virtualenv python-pip apache2 # You may need a later version of nodejs if you run into a "Method not found error"
+wget http://cesiumjs.org/releases/Cesium-1.23.zip
+mkdir cesium
+unzip Cesium-1.23.zip -d cesium
+sudo cp -R cesium /var/www/html/
 git clone https://github.com/tordanik/OSM2World.git
 git clone https://github.com/AnalyticalGraphicsInc/OBJ2GLTF.git
 virtualenv ~/venv
