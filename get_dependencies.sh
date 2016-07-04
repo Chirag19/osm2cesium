093077d8db33cfd4120cf3aa83f4197d6d07f535
stirringhalo <stirringhalo@users.noreply.github.com>
2016-07-04 14:00:25 -0400

    Add git

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -1,6 +1,6 @@
 # Works on Ubuntu 16.04 at least
 
-sudo apt-get install osmosis nodejs python-virtualenv python-pip apache2 # You may need a later version of nodejs if you run into a "Method not found error"
+sudo apt-get install osmosis nodejs python-virtualenv python-pip apache2 git # You may need a later version of nodejs if you run into a "Method not found error"
 wget http://cesiumjs.org/releases/Cesium-1.23.zip
 mkdir cesium
 unzip Cesium-1.23.zip -d cesium
